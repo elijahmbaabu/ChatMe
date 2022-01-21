45fe4a3a723ca5bc47560545b62cd09eacd1f71b
Elijah Mbaabu <elijahmbaabu1@gmail.com>
2022-01-21 16:33:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/chatme/Splash.java
@@ -0,0 +1,32 @@
+package com.example.chatme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    private  static  int screen_timer = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, screen_timer);
+
+    }
+}
